1fd03d3e8b3af52d7da0337b82f58be48c48fcf0
hoangtrunga1k55 <hoangtrunga1k55@gmail.com>
2019-05-03 23:14:34 +0700

    change user

---
+++ DB_Management.php
@@ -0,0 +1,50 @@
+<?php 
+class DB_Management {
+	private $__conn;
+
+	public function connectDb() {
+		if(!$this->$__conn) {
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "post_management";
+
+			$this->$__conn = new mysqli($servername, $username, $password, $dbname);
+			
+			if ($this->$__conn->connect_error) {
+			    die("__connection failed: " . $this->$__conn->connect_error);
+			} 
+		}
+	}
+
+	public function disconnect(){
+		$this->$__conn->close();
+	}
+
+	public function createUser($data) {
+		$name = $data['name'];
+		$email = $data['email'];
+		$city = $data['city'];
+		$password = $data['password'];
+
+		$this->connectDb();
+
+		$sql = "INSERT INTO `users`(`user_id`, `name`, `email`, `city`, `password`) 
+						VALUES (null,'$name','$email','$city','$password')";
+
+		return $this->$__conn->query($sql);
+	}
+
+	public function checkUser($data) {
+		$email = $data["email"];
+		$pass = $data["password"];
+
+		$this->connectDb();
+
+		$sql = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
+		$result = $this->$__conn->query($sql);
+		
+		return $result->num_rows > 0;
+	}
+} 
+?>
--- Model/User.php
+++ Model/User.php
@@ -1,62 +1,18 @@
 <?php
 
+$DIR_FILE = 'D:\mysql\htdocs\post_management';
+include($DIR_FILE. '\DB_Management.php');
+
 class User
 {
 	public function create($data) {
-		$name = $data['name'];
-		$email = $data['email'];
-		$city = $data['city'];
-		$password = $data['password'];
-
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "post_management";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		}		
-
-		$sql = "INSERT INTO `users`(`user_id`, `name`, `email`, `city`, `password`) 
-						VALUES (null,'$name','$email','$city','$password')";
-
-		if ($conn->query($sql) === TRUE) {
-		   	return true;
-		} else {
-		    return false;
-		}
+		$db = new DB_Management();
+		return $db->createUser($data);
 	}	
 
 	public function checkUser($data){
-		$email = $data["email"];
-		$pass = $data["password"];
-
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "post_management";
-
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		} 
-
-		$sql = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
-		$result = $conn->query($sql);
-		
-		if ($result->num_rows > 0) {
-	    	return true;
-	    }else {
-    		return false;
-		}
-
-		$conn->close();
+		$db = new DB_Management();
+		return $db->checkUser($data);
 	}
 
 }
